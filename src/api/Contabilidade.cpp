0fd268d98510916216867fb646772ce39ed3846d
Lucas Ribeiro <lmr2199@gmail.com>
2020-10-07 22:51:05 -0300

    Basic Templeate

---
+++ src/api/Contabilidade.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lucas on 07/10/2020.
+//
+
+#include "Contabilidade.h"
---
+++ src/app/QChicken_pt_BR.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="QChicken_pt_BR">
+<context>
+    <name>QChickenMain</name>
+    <message>
+        <location filename="qchickenmain.ui" line="14"/>
+        <location filename="../../cmake-build-debug/QChicken_autogen/include/ui_qchickenmain.h" line="56"/>
+        <source>QChickenMain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qchickenmain.ui" line="27"/>
+        <location filename="../../cmake-build-debug/QChicken_autogen/include/ui_qchickenmain.h" line="57"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
